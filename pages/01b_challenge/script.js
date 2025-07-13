3a1012e2a78edfc602dec19dda39340c04ba75ec
Sanu-Ranjan <ranjan.code33@gmailcom>
2025-07-14 00:07:44 +0530

    commiting from new pc

--- pages/01b_challenge/script.js
+++ pages/01b_challenge/script.js
@@ -1,7 +1,7 @@
 import gsap from "gsap";
 
 gsap.to(".card", {
-  boxShadow: "0 0 80px rgba(29, 209, 161, 0.6)",
+  boxShadow: "0 0 80px rgba(29, 209, 169, 0.6)",
   repeat: -1,
   yoyo: true,
   duration: 1,
